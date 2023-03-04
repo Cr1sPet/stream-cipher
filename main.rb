1f0c9dc43a596f4b5fdf658d5e452490f955e8e7
Cr1sPet <serezha.petrov.02@gmail.com>
2023-03-04 17:53:26 +0300

    генерация М-последовательности

--- main.rb
+++ main.rb
@@ -4,24 +4,47 @@ require_relative 'crypto'
 require 'debug'
 include StreamCipher
 
-@arr = []
+KEY_FILE = 'key.txt'
 
-def start(l, s)
-  state = count = 2**l - 1
-
-  puts "%0#{l}b" % state
+def m_sequence_generate(len, state, cs)
+  count = 2**len - 1
+  @arr = []
   @arr << state[0]
+  cache = Hash.new(false)
+  cache[state] = true
   count.times do
-    first_digit = state[l - 1]
-    last_digit = state[s - 1]
-    result = first_digit ^ last_digit
-    state = (state >> 1) | (result << l - 1)
+    digits = cs.map { |c| state[c - 1] }
+    result = digits.inject(:^)
+    state = (state >> 1) | (result << len - 1)
+    if cache[state] == true
+      break
+    end
+    cache[state] = true
     @arr << state[0]
-    puts "%0#{l}b" % state
   end
-  print @arr
+  @arr
+  File.write('M-seq.txt', @arr)
 end
 
-l = 5
-s = 2
-start(l, s)
+def state_generate(len)
+  rand(2**(len - 1)...2**len)
+end
+
+def read_key
+  File.read(KEY_FILE).strip
+end
+
+def start(len, cs)
+  if ARGV.include? 'read'
+    key = read_key
+    len = key.length
+  end
+  state = key&.to_i(2) || state_generate(len)
+  File.write(KEY_FILE, state.to_s(2))
+  m_sequence_generate(len, state, cs)
+end
+
+len = 25
+
+cs = [23, 9]
+start(len, cs)
