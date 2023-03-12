daeb5078be67589032158a60a0f65ea218b501da
Cr1sPet <serezha.petrov.02@gmail.com>
2023-03-12 16:51:31 +0300

    рефакторинг

--- main.rb
+++ main.rb
@@ -1,51 +1,55 @@
 # frozen_string_literal: true
 
-require_relative 'crypto'
 require 'debug'
-include StreamCipher
 
 KEY_FILE = 'key.txt'
+M_SEQ_FILE = 'M-seq.txt'
 
-def m_sequence_generate(len, state, cs)
-  count = 2**len - 1
-  @arr = []
-  @arr << state[0]
-  cache = Hash.new(false)
-  cache[state] = true
+def generate_m_sequence(length, state, feedback_bits)
+  count = 2**length - 1
+  arr = [state[0]]
+  cache = { state => true }
+
   count.times do
-    digits = cs.map { |c| state[c - 1] }
+    digits = feedback_bits.map { |bit| state[bit - 1] }
     result = digits.inject(:^)
-    state = (state >> 1) | (result << len - 1)
-    break if cache[state] == true
+    state = (state >> 1) | (result << length - 1)
+    break if cache[state]
 
     cache[state] = true
-    @arr << state[0]
+    arr << state[0]
   end
-  File.write('M-seq.txt', @arr)
-  @arr
+
+  File.write(M_SEQ_FILE, arr)
+  arr
 end
 
-def state_generate(len)
-  rand(2**(len - 1)...2**len)
+def generate_state(length)
+  rand(2**(length - 1)...2**length)
 end
 
 def read_key
   File.read(KEY_FILE).strip
 end
 
-def pirson_criterion(_n_teor, _n_prac, k)
-  2**k.times do
+def pirson_criterion(psi, k)
+  case k
+  when 2
+    psi >= 0.584 && psi <= 6.251
+  when 3
+    psi >= 2.833 && psi <= 12.017
+  when 4
+    psi >= 8.547 && psi <= 22.307
   end
 end
 
-def n_teor(m_sequence, block_length)
-  n_teor = m_sequence.length / (2**block_length * block_length.to_f)
+def theoretical_n(m_sequence, block_length)
+  m_sequence.length / (2**block_length * block_length.to_f)
 end
 
-def psi(n_teor, m_series_counts)
+def calculate_psi(theoretical_n, m_series_counts)
   m_series_counts.values.sum do |count|
-    puts count
-    ((count - n_teor)**2) / n_teor.to_f
+    ((count - theoretical_n)**2) / theoretical_n.to_f
   end
 end
 
@@ -53,32 +57,39 @@ def serial_test(m_sequence, block_length)
   m_series = m_sequence.each_slice(block_length).to_a
   m_series = m_series[0, (m_series.size - 1)] unless m_series.last.length == block_length
   m_series_counts = Hash.new(0)
+
   m_series.each do |seq|
     m_series_counts[seq] += 1
   end
-  m_series_frequencies = m_series_counts.each.each_with_object({}) do |el, memo|
-    memo[el[0]] = el[1] / (m_sequence.length.to_f / block_length).to_f
+
+  m_series_frequencies = m_series_counts.transform_values do |count|
+    count / (m_sequence.length.to_f / block_length).to_f
   end
+
+  theoretical_n = theoretical_n(m_sequence, block_length)
+  psi = calculate_psi(theoretical_n, m_series_counts)
+
   puts m_series_counts
   puts m_series_frequencies
-  n_teor = n_teor(m_sequence, block_length)
-  puts n_teor
-  puts psi(n_teor, m_series_counts)
+  puts theoretical_n
+  puts psi
+  puts "Pirson criterion result: #{pirson_criterion(psi, block_length)}"
 end
 
-def start(len, cs)
-  if ARGV.include? 'read'
+def start(length, feedback_bits, block_length)
+  if ARGV.include? '--mRead'
     key = read_key
-    len = key.length
+    length = key.length
   end
-  state = key&.to_i(2) || state_generate(len)
+
+  state = key&.to_i(2) || generate_state(length)
   File.write(KEY_FILE, state.to_s(2))
-  m_seq = m_sequence_generate(len, state, cs)
-  serial_test(m_seq, 3)
+  m_sequence = generate_m_sequence(length, state, feedback_bits)
+  serial_test(m_sequence, block_length)
 end
 
-len = 25
+length = 25
+feedback_bits = [23, 9]
+block_length = 2
 
-cs = [23, 9]
-
-start(len, cs)
+start(length, feedback_bits, block_length)
